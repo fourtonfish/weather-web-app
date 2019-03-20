3e0217a9973e549f4e1364eae9ed18661e7bba23
Glitch (weather-web-app) <none>
2019-03-20 03:19:27 +0000

    Backup from Glitch.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# weather-web-app
-A simple weather web app.
+![Weather web app showing weather in Brooklyn](https://fourtonfish.com/wp-content/uploads/2019/03/web-apis-tutorial-06-app-finished-700x411.png)
+
+# Weather web app
+
+A weather web app powered by [OpenWeatherMap](http://openweathermap.org), [Flickr](http://flickr.com/services/api/), and [Glitch](https://glitch.com).
+
+Make your own weather web app [with my tutorial](https://fourtonfish.com/blog/weather-web-app-web-api-tutorial/)  (coming soon).
---
+++ script.js
@@ -0,0 +1,48 @@
+function ready( fn ) {
+  if ( document.attachEvent ? document.readyState === 'complete' : document.readyState !== 'loading' ){
+    fn();
+  } else {
+    document.addEventListener( 'DOMContentLoaded', fn );
+  }
+}
+
+ready( function(){
+  function makeApiCall( url, callback ){
+    var xhr = new XMLHttpRequest();
+
+    xhr.addEventListener( 'load', function(){
+      var response = JSON.parse(xhr.responseText);
+      if ( callback ){
+        callback( response );
+      }
+    });
+
+    xhr.open( 'GET', url );
+    xhr.send();
+  }
+
+  makeApiCall( 'https://location-weather-api.glitch.me/weather?api_key=1c66c7e9-129e-49b1-95de-f4f5d706d914', function( data ){
+    
+    var location = data.location.city + ', ' + data.location.region;
+    var weather = data.weather.weather[0].main;
+    var weatherDescription = data.weather.weather[0].description;
+    var temperature = data.weather.main.temp;
+    
+    document.getElementById( 'city' ).innerHTML = location;
+    document.getElementById( 'weather' ).innerHTML = Math.round( temperature ) + '°F, ' + weatherDescription;
+
+    makeApiCall( 'https://location-weather-api.glitch.me/background?api_key=1c66c7e9-129e-49b1-95de-f4f5d706d914&weather=' + weather + '&latitude=' + data.location.lat + '&longitude=' + data.location.lon, function( data ){
+      var imageSourceLink = document.getElementById( 'image-source' );
+      
+      if (data && data.photos && data.photos.pages > 0){
+				var photo = data.photos.photo[0];
+				document.body.style.backgroundImage = "url('" + photo.url_l + "')";
+				imageSourceLink.setAttribute( 'href', 'http://www.flickr.com/photos/' + photo.owner + '/' + photo.id );
+			}
+			else{
+				document.body.style.backgroundImage = 'url("https://fourtonfish.com/tutorials/weather-web-app/images/default.jpg")';
+				imageSourceLink.setAttribute( 'href', 'https://www.flickr.com/photos/superfamous/310185523/sizes/o/' );
+			}      
+    });
+  });
+});
